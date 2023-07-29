f12ecaa8d7233b61d23abc09024dacabba1077d0
Julien Marseille <julien@squadracer.com>
2023-07-29 05:50:41 +0200

    yml_locales in front

--- app/controllers/locale_ninja/locales_controller.rb
+++ app/controllers/locale_ninja/locales_controller.rb
@@ -2,6 +2,8 @@
 
 module LocaleNinja
   class LocalesController < ApplicationController
-    def index; end
+    def index
+      @yml_locales = GithubService.call.map{YAML.load(_1)}
+    end
   end
 end
