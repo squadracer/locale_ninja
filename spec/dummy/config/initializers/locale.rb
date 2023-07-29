6e512ed83408cb279f72be458da09a431385146d
Julien Marseille <julien@squadracer.com>
2023-07-29 05:54:42 +0200

    rubocop

--- spec/dummy/config/initializers/locale.rb
+++ spec/dummy/config/initializers/locale.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 # Where the I18n library should search for translation files
-I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+I18n.load_path += Dir[Rails.root.join('lib/locale/*.{rb,yml}')]
 
 # Permitted locales available for the application
-I18n.available_locales = [:en, :fr]
+I18n.available_locales = %i[en fr]
 
 # Set default locale to something other than :en
 I18n.default_locale = :fr
