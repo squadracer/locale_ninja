d3861fde6d6b68420c0486f7a07203c12120afa8
Théo Dupuis <theo@squadracer.com>
2023-07-29 17:04:07 +0200

    refacto

--- app/services/locale_ninja/github_service.rb
+++ app/services/locale_ninja/github_service.rb
@@ -14,11 +14,14 @@ module LocaleNinja
       @client = Octokit::Client.new(access_token:)
     end
 
-    def call
+    def local_files_path
       repository_name = @client.repositories.find { |repo| repo[:name] == REPOSITORY_NAME }[:full_name]
       repository = Octokit::Repository.new(repository_name)
-      locale_files_path = @client.contents(repository, path: 'config/locales').map(&:path)
-      locale_files_path.map { |path| Base64.decode64(@client.contents(repository, path:).content) }
+      @client.contents(repository, path: 'config/locales').map(&:path)
+    end
+
+    def pull
+      local_files_path.map { |path| Base64.decode64(@client.contents(repository, path:).content) }
     end
 
     def push(file_path, content)
