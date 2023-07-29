d12a3650e970678b2c912362d0430918b869a380
Théo Dupuis <theo@squadracer.com>
2023-07-29 03:49:01 +0200

    lol, j'ai laissé le puts

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -9,7 +9,6 @@ class GithubService
     repository_name = client.repositories.find { |repo| repo[:name] == REPOSITORY_NAME }[:full_name]
     repository = Octokit::Repository.new(repository_name)
     locale_files_path = client.contents(repository, path:'config/locales').map(&:path)
-    locale_files = locale_files_path.map { |path| Base64.decode64(client.contents(repository, path: path).content) }
-    puts locale_files.join("\n\n")
+    locale_files_path.map { |path| Base64.decode64(client.contents(repository, path: path).content) }
   end
 end
