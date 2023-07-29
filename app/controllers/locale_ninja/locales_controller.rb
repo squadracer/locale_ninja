1c78803e7e6a8198677fe01f7b9cf7e0aa24864a
Julien Marseille <julien@squadracer.com>
2023-07-29 04:14:36 +0200

    remove show page on locales

--- app/controllers/locale_ninja/locales_controller.rb
+++ app/controllers/locale_ninja/locales_controller.rb
@@ -3,7 +3,5 @@
 module LocaleNinja
   class LocalesController < ApplicationController
     def index; end
-
-    def show; end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 LocaleNinja::Engine.routes.draw do
-  resources :locales, only: %i[index show], path: '/'
+    resources :locales, only: [:index], path: "/"
 end
