7993f5d77c9d5fc57861dc0b932c223877581fc8
Julien Marseille <julien@squadracer.com>
2023-07-29 15:35:54 +0200

    view card on translations

--- app/controllers/locale_ninja/locales_controller.rb
+++ app/controllers/locale_ninja/locales_controller.rb
@@ -3,12 +3,27 @@
 module LocaleNinja
   class LocalesController < ApplicationController
     def index
-      @yml_locales = LocaleNinja::GithubService.call.map { YAML.load(_1) }
+      locales_yml = LocaleNinja::GithubService.call.map { YAML.load(_1) }
+
+      @code_value_by_locales = locales_yml.to_h { [_1.keys[0], traverse(_1)] }
     end
 
     def github
       # pour voir le retour de la github app
       @content = params.inspect
+    end
+
+    def traverse(hash, parent_key = nil)
+      path = []
+      hash.each do |key, value|
+        current_key = parent_key ? "#{parent_key}.#{key}" : key.to_s
+        if value.is_a?(Hash)
+          path += traverse(value, current_key)
+        else
+          path << [current_key, value]
+        end
+      end
+      path
     end
   end
 end
