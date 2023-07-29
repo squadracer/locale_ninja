d84c92369618f248f29f3326b98489453a310486
Théo Dupuis <theo@squadracer.com>
2023-07-29 09:08:57 +0200

    prepare routes for github app

--- app/controllers/locale_ninja/locales_controller.rb
+++ app/controllers/locale_ninja/locales_controller.rb
@@ -5,5 +5,10 @@ module LocaleNinja
     def index
       @yml_locales = GithubService.call.map { YAML.load(_1) }
     end
+
+    def github
+      # pour voir le retour de la github app
+      @content1 = params.inspect
+    end
   end
 end
