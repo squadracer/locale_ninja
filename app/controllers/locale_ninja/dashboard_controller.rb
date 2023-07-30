790ac17fbed5f8ce3265a5851a6ac917d05f6998
Théo Dupuis <theo@squadracer.com>
2023-07-30 22:22:42 +0200

    only count translation branches

--- app/controllers/locale_ninja/dashboard_controller.rb
+++ app/controllers/locale_ninja/dashboard_controller.rb
@@ -6,8 +6,8 @@ module LocaleNinja
 
     def index
       @locales_count = LocaleHelper.locales_count(@client)
-      @branches_count = @client.branches.count
-      translation_branches = @client.branches.filter { |branch| branch.ends_with?('__translations') }
+      @branches_count = translation_branches = @client.branches.count { |branch| branch.ends_with?('__translations') }
+
       @commits_count = translation_branches.sum do |branch|
         @client.client.commits_since(@client.repository_fullname,
                                      1.month.ago.strftime('%Y-%m-%d'),
