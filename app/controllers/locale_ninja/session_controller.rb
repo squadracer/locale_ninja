47ab1c3f4cd272c90bd23951d6967fd085521d8d
Théo Dupuis <theo@squadracer.com>
2023-07-30 21:47:36 +0200

    fix: indent

--- app/controllers/locale_ninja/session_controller.rb
+++ app/controllers/locale_ninja/session_controller.rb
@@ -13,12 +13,12 @@ module LocaleNinja
     def login
       code = params['code']
       response = ::HTTParty.post('https://github.com/login/oauth/access_token',
-                               body: {
-                                 client_id: CLIENT_ID,
-                                 client_secret: CLIENT_SECRET,
-                                 code:
-                               }
-                              )
+                                 body: {
+                                   client_id: CLIENT_ID,
+                                   client_secret: CLIENT_SECRET,
+                                   code:
+                                 }
+                                )
       parsed = CGI.parse(response)
       session[:access_token] = parsed['access_token'].first
       user = GithubApiService.new(access_token:).user
