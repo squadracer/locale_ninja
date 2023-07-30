ef59e2b5c459df6cd7a7f9970c71e90af1e920a3
Clement Avenel <clement@squadracer.com>
2023-07-30 06:29:32 +0200

    make linter a happy guy

--- app/helpers/locale_ninja/locale_helper.rb
+++ app/helpers/locale_ninja/locale_helper.rb
@@ -2,10 +2,9 @@
 
 module LocaleNinja
   module LocaleHelper
-
     def self.keys2yml(translation_keys)
       translation_keys.each_with_object({}) do |(key, value), hash|
-        hash.deep_merge!(key.split('.').reverse.inject(value) { |a, n| { n => a } })
+        hash.deep_merge!(key.split('.').reverse.reduce(value) { |a, n| { n => a } })
       end.to_yaml
     end
   end
