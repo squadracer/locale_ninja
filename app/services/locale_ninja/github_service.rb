eab9fe461fe59a8be8d85a1e9f0fc5387bb0be34
Clement Avenel <clement@squadracer.com>
2023-07-30 10:01:22 +0200

    add file if not exist

--- app/services/locale_ninja/github_service.rb
+++ app/services/locale_ninja/github_service.rb
@@ -28,8 +28,17 @@ module LocaleNinja
     end
 
     def push(file_path, content)
-      sha = @client.content(repository_fullname, path: file_path, ref: 'heads/translations')[:sha]
+      begin
+        sha = @client.content(repository_fullname, path: file_path, ref: 'heads/translations')[:sha]
+      rescue Octokit::NotFound
+        create_file(file_path, content,branch: 'translations')
+        return
+      end
       @client.update_contents(repository_fullname, file_path, "translations #{DateTime.current}", sha, content, branch: 'translations')
+    end
+
+    def create_file(file_path, content, branch: 'translations')
+      @client.create_contents(repository_fullname, file_path, "translations #{DateTime.current}", content, branch: branch)
     end
 
     def create_branch(parent_branch, child_branch)
