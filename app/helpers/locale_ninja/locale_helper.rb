2c81a09ea2755c379d74b2cacf0c5265c0110ae9
Clement Avenel <clement@squadracer.com>
2023-07-30 06:27:16 +0200

    locale update push to github

--- app/helpers/locale_ninja/locale_helper.rb
+++ app/helpers/locale_ninja/locale_helper.rb
@@ -2,5 +2,11 @@
 
 module LocaleNinja
   module LocaleHelper
+
+    def self.keys2yml(translation_keys)
+      translation_keys.each_with_object({}) do |(key, value), hash|
+        hash.deep_merge!(key.split('.').reverse.inject(value) { |a, n| { n => a } })
+      end.to_yaml
+    end
   end
 end
