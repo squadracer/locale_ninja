ca905cb4f80021ed4883e507ff263a0625e529d5
Théo Dupuis <theo@squadracer.com>
2023-07-30 17:25:00 +0200

    push to __translations branch

--- app/services/locale_ninja/github_api_service.rb
+++ app/services/locale_ninja/github_api_service.rb
@@ -12,23 +12,42 @@ module LocaleNinja
       @client = Octokit::Client.new(access_token:)
     end
 
+    def translation_branch(branch)
+      branch.ends_with?('__translations') ? branch : "#{branch}__translations"
+    end
+
     def locale_files_path(dir = 'config/locales', branch: 'translations')
+      branch = translation_branch(branch) if branch?(translation_branch(branch))
       @client.contents(repository_fullname, path: dir, ref: "heads/#{branch}").map do |file|
         if file.type == 'dir'
-          locale_files_path(file.path)
+          locale_files_path(file.path, branch:)
         else
           file.path
         end
       end.flatten
     end
 
-    def pull(file = locale_files_path, branch: 'translations')
-      branch = 'main' unless branch?(branch)
-      file.index_with { |path| Base64.decode64(@client.contents(repository_fullname, path:, ref: "heads/#{branch}").content) }
+    def pull(files = nil, branch: 'translations')
+      files ||= locale_files_path(branch:)
+      branch = translation_branch(branch) if branch?(translation_branch(branch))
+      raise('This branch does not exist') unless branch?(branch)
+
+      files.index_with { |path| Base64.decode64(@client.contents(repository_fullname, path:, ref: "heads/#{branch}").content) }
+    end
+
+    def create_translation_branch(branch)
+      return branch if branch.ends_with?('__translations')
+
+      translation_branch = "#{branch}__translations"
+      return translation_branch if branch?(translation_branch)
+
+      create_branch(branch, translation_branch)
+      translation_branch
     end
 
     def push(file_path, content, branch: 'translations')
-      create_branch('main', branch) unless branch?(branch)
+      branch = create_translation_branch(branch)
+
       begin
         sha = @client.content(repository_fullname, path: file_path, ref: "heads/#{branch}")[:sha]
       rescue Octokit::NotFound
@@ -45,12 +64,11 @@ module LocaleNinja
     def create_branch(parent_branch, child_branch)
       sha = @client.ref(repository_fullname, "heads/#{parent_branch}").dig(:object, :sha)
       @client.create_ref(repository_fullname, "heads/#{child_branch}", sha)
+      @branches << child_branch
     end
 
     def branch?(branch_name)
-      @client.ref(repository_fullname, "heads/#{branch_name}")
-    rescue Octokit::NotFound
-      nil
+      branches.include?(branch_name)
     end
 
     def repository_fullname
@@ -58,11 +76,11 @@ module LocaleNinja
     end
 
     def branches
-      @client.branches(repository_fullname).map(&:name)
+      @branches ||= @client.branches(repository_fullname).map(&:name)
     end
 
     def pull_request(branch_name)
-      @client.create_pull_request(repository_fullname, 'main', branch_name, "translations #{Time.current}")
+      @client.create_pull_request(repository_fullname, branch_name, "#{branch_name}__translations", "translations #{Time.current}")
     rescue Octokit::UnprocessableEntity
       # If pull request already exists, do nothing
     end
