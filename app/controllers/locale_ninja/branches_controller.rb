e157caaf234ba4007a944f369c4e95954e437a5f
pfitoussi <fitoussi.pierre@gmail.com>
2023-07-30 23:58:07 +0200

    feat: add links

--- app/controllers/locale_ninja/branches_controller.rb
+++ app/controllers/locale_ninja/branches_controller.rb
@@ -13,7 +13,7 @@ module LocaleNinja
     end
 
     def show
-      @branches = @client.public_branches
+      @branches = @client.public_branch_names
       @branch_name = params[:id]
       locales_yml = @client.pull(branch: @branch_name).values.map { YAML.load(_1) }
       @code_value_by_locales = locales_yml.to_h { [_1.keys[0], LocaleHelper.traverse(_1)] }
