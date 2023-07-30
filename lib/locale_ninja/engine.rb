de998e677e047dc1fdde737a4283724139abd83e
Clement Avenel <clement@squadracer.com>
2023-07-30 13:09:28 +0200

    Clean Engine assets precompile

--- lib/locale_ninja/engine.rb
+++ lib/locale_ninja/engine.rb
@@ -5,7 +5,7 @@ module LocaleNinja
     isolate_namespace LocaleNinja
 
     initializer 'locale_ninja.assets' do |app|
-      app.config.assets.precompile += %w[locale_ninja_manifest locale_ninja/application.css locale_ninja/application.js locale_ninja.css]
+      app.config.assets.precompile += %w[locale_ninja_manifest]
     end
   end
 end
