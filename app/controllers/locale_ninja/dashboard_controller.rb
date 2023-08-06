e0adb63192ed23cecfac28240ca1559e41a706ab
Clement Avenel <clement@squadracer.com>
2023-08-07 00:24:43 +0200

    Refactor github_api_service & fix Dashboard locales_count

--- app/controllers/locale_ninja/dashboard_controller.rb
+++ app/controllers/locale_ninja/dashboard_controller.rb
@@ -5,12 +5,12 @@ module LocaleNinja
     before_action :set_client, only: [:index]
 
     def index
-      @locales_count = LocaleHelper.locales_count(@client)
+      @locales_count = LocaleHelper.locales_count(@client, @client.default_branch)
       @repo = @client.repo_information
-      translation_branches = @client.branches.filter { |branch| branch[:name].ends_with?('__translations') }
+      translation_branches = @client.branch_names.filter { |branch_name| branch_name.ends_with?(GithubApiService::TRANSLATIONS_SUFFIX) }
       @branches_count = @client.branches.count - translation_branches.count
       @commits_count = translation_branches.sum do |branch|
-        @client.client.commits_since(@client.repository_fullname,
+        @client.client.commits_since(GithubApiService::REPOSITORY_FULLNAME,
                                      1.month.ago.strftime('%Y-%m-%d'),
                                      sha_or_branch: branch
                                     ).count
